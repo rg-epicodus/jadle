27a46264152aaa5f43ed41b60588ea93e12b227e
Rich Garrick <richg341@gmail.com>
2017-08-21 15:30:14 -0700

    setup sql2o connections for tests

--- src/test/java/dao/Sql2oRestaurantDaoTest.java
+++ src/test/java/dao/Sql2oRestaurantDaoTest.java
@@ -2,16 +2,31 @@ package dao;
 
 import org.junit.After;
 import org.junit.Before;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.Assert.*;
 
 public class Sql2oRestaurantDaoTest {
+    private Connection conn;
+    private Sql2oRestaurantDao restaurantDao;
+    private Sql2oFoodtypeDao foodtypeDao;
+
+
+
     @Before
     public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        restaurantDao = new Sql2oRestaurantDao(sql2o);
+        foodtypeDao = new Sql2oFoodtypeDao(sql2o);
+        conn = sql2o.open();
+
     }
 
     @After
     public void tearDown() throws Exception {
+        conn.close();
     }
 
 }
--- src/test/java/dao/Sql2oReviewDaoTest.java
+++ src/test/java/dao/Sql2oReviewDaoTest.java
@@ -2,16 +2,29 @@ package dao;
 
 import org.junit.After;
 import org.junit.Before;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.Assert.*;
 
 public class Sql2oReviewDaoTest {
+    private Connection conn;
+    private Sql2oReviewDao reviewDao;
+    private Sql2oRestaurantDao restaurantDao;
+
     @Before
     public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        reviewDao = new Sql2oReviewDao(sql2o);
+        restaurantDao = new Sql2oRestaurantDao(sql2o);
+        conn = sql2o.open();
+
     }
 
     @After
     public void tearDown() throws Exception {
+        conn.close();
     }
 
 }
